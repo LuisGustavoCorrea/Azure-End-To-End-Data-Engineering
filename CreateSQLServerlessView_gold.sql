7eafc647b33a0077867fd5b808079a7990bc95a7
LuisGustavoCorrea <luis_gustavo_correa@hotmail.com>
2024-06-24 17:44:40 -0300

    Add files via upload

---
+++ CreateSQLServerlessView_gold.sql
@@ -0,0 +1,21 @@
+USE gold_db
+GO
+
+CREATE OR ALTER PROC CreateSQLServerlessView_gold @ViewName nvarchar(100)
+AS 
+BEGIN
+
+
+DECLARE @statement VARCHAR(MAX)
+
+    SET @statement = N'CREATE OR ALTER VIEW ' + @ViewName + ' AS 
+                        SELECT * FROM
+    OPENROWSET(
+        BULK ''https://gusdatalakegen2.dfs.core.windows.net/gold/SalesLT/' + @ViewName + '/'',
+        FORMAT = ''DELTA''
+    ) AS [result]'
+
+EXEC (@statement)
+
+END 
+GO
